73a5cb7daa407a681c31543106866014699a2cfb
Aneeshbs44 <anish107655@gmail.com>
2024-11-19 17:18:17 +0530

    Fixed the orientation of LOGIN heading and other fields

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -33,6 +33,11 @@ const Login = () => {
           placeholder="Enter your Email"
           placeholderTextColor="#999"
         />
+        <TextInput
+          style={styles.field1}
+          placeholder="Enter your Password"
+          placeholderTextColor="#999"
+        />
 
         {/* Button */}
         <View style={styles.buttonContainer}>
@@ -52,12 +57,12 @@ const styles = StyleSheet.create({
     width: '100%',
     position: 'absolute',
     top: 0,
-    zIndex: 1, // Place it behind the text
+    zIndex: 2, // Place it behind the text
   },
   content: {
     flex: 1,
-    marginTop: screenHeight / 3, // Push content below the image
-    paddingHorizontal: 20,
+    marginTop: screenHeight / 2.4, // Push content below the image
+    paddingHorizontal: 35,
     zIndex: 2, // Ensure content is above the image
   },
   heading: {
@@ -73,11 +78,20 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     height: 50,
     paddingHorizontal: 15,
+    marginTop: screenHeight / 3000,
     marginBottom: 20,
+  },
+  field1:{
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    height: 50,
+    paddingHorizontal: 15,
+    marginTop: screenHeight / 3000,
   },
   buttonContainer: {
     alignItems: 'center',
-    marginTop: 20,
+    marginTop: 60,
   },
 });
 
